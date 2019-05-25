d228b940746ce8b5f22fa71dee16a5c835d3e582
hicham <elkhaldihicham@gmail.com>
2019-05-25 14:37:05 +0000

    http services and error handling

--- src/posts.jsx
+++ src/posts.jsx
@@ -1,28 +1,30 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/http-service";
+import config from "./services/config.json";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const EndPoint = "https://jsonplaceholder.typicode.com/posts";
 class Post extends Component {
   state = {
     posts: []
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get(EndPoint);
+    const { data: posts } = await http.get(config.EndPoint);
     this.setState({ posts });
   }
 
   handelAdd = async () => {
     const obj = { title: "new post", body: "this post from axios " };
-    const { data: post } = await axios.post(EndPoint, obj);
+    const { data: post } = await http.post(config.EndPoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
   };
 
   handeleUpdate = async post => {
     post.title = "Updated";
-    await axios.put(EndPoint + "/" + post.id, post);
-    //await axios.patch(EndPoint + "/" + post.id, { title: post.title });
+    await http.put(config.EndPoint + "/" + post.id, post);
+    //await http.patch(config.EndPoint + "/" + post.id, { title: post.title });
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -31,15 +33,24 @@ class Post extends Component {
   };
 
   handeleDelete = async post => {
-    await axios.delete(EndPoint + "/" + post.id);
+    const originalPosts = this.state.posts;
 
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
+    try {
+      await http.delete("s" + config.EndPoint + "/" + post.id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        alert("Post has already been deleted");
+
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
     return (
       <div className="container">
+        <ToastContainer />
         <button className="btn btn-primary" onClick={this.handelAdd}>
           Add
         </button>
